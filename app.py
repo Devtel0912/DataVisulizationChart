5f5ecf95bf23d764c86e08a8a471778b29ea3591
Devtel0912 <devtel0912@gmail.com>
2026-01-07 09:18:43 -0500

    Data Visulizartion

---
+++ app.py
@@ -0,0 +1,31 @@
+import random
+from unicodedata import category
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+import os
+
+app = FastAPI(title="Data Visualization App")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def serve_index():
+    index_file = os.path.join("static", "index.html")
+    with open(index_file, "r", encoding="utf-8") as file:
+        return file.read()
+    
+data = {
+    day: {
+        "online": random.randint(1000, 5000),
+        "offline": random.randint(1000, 5000)
+    }
+    for day in ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+}
+
+@app.get("/data/{name}")
+def get_data_by_name(name: str):
+    if name not in data:
+        raise HTTPException(status_code=404, detail="Not found")
+
+    return data[name]
